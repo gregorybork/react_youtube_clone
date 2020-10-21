062463645d32b366906542b42bd7ea744415fd52
gregorybork <gregorybork1@gmail.com>
2020-10-20 20:09:33 -0700

    Updates

---
+++ precache-manifest.8518f343da63b2de73318e71a61a857e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a636f9e1f636f1627e93aa25d66e0539",
+    "url": "/react_youtube_clone/index.html"
+  },
+  {
+    "revision": "e9118697264d82f4c715",
+    "url": "/react_youtube_clone/static/css/main.47378431.chunk.css"
+  },
+  {
+    "revision": "ea7da8e4f87b4b29b6e1",
+    "url": "/react_youtube_clone/static/js/2.0cd9a135.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react_youtube_clone/static/js/2.0cd9a135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9118697264d82f4c715",
+    "url": "/react_youtube_clone/static/js/main.a03a8919.chunk.js"
+  },
+  {
+    "revision": "568c752d1a524704561e",
+    "url": "/react_youtube_clone/static/js/runtime-main.a8b0812a.js"
+  }
+]);
